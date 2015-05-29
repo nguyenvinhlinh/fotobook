1ac50f683142e457eb05cb3d5869493f8a7a66d5
nguyenvinhlinh <nguyenvinhlinh93@gmail.com>
2015-05-29 10:17:51 +0700

    fixed validation of tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,7 @@ class Tag < ActiveRecord::Base
   has_and_belongs_to_many :pictures
   accepts_nested_attributes_for :pictures
   validates :tag,
-            absense: true,
+            absence: true,
             length: {
               minium: 2,
               maximum: 10
