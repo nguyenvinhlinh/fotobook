085b36114aaedf39e4b9df57209e8e4e167fe52f
nguyenvinhlinh <nguyenvinhlinh93@gmail.com>
2015-07-06 13:47:05 +0700

    first commit for testing

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -9,6 +9,7 @@ class PicturesController < ApplicationController
     page_number = 1 if page_number == nil
     if @page_tags == nil || @page_tags == ""
       @pictures = Picture.page(page_number).per(11)
+      
     else
       tag_array = @page_tags.split(",").map(&:strip).uniq
       tag_array.delete ""
