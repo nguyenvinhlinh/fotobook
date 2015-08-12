4aba2bbb43278073f472015e62d9231b2f5e9033
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2015-08-12 16:54:13 +0700

    init token before refresh token

--- app/workers/imgur_worker.rb
+++ app/workers/imgur_worker.rb
@@ -2,6 +2,7 @@ class ImgurWorker
   include Sidekiq::Worker
   REFREST_TIME = 45 # 45 minutes
   def perform
+    Imgur::initTokens
     while (true)
       ImgurApi::refreshTokens
       sleep 45 * 60
