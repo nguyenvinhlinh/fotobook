b17bd193bf175dabf3f4191edfc3728743cde344
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2015-08-12 19:09:56 +0700

    refresh token works well

--- app/workers/imgur_worker.rb
+++ app/workers/imgur_worker.rb
@@ -1,11 +1,10 @@
+require 'imgur-api'
 class ImgurWorker
   include Sidekiq::Worker
+  include ImgurApi
   REFREST_TIME = 45 # 45 minutes
   def perform
-    Imgur::initTokens
-    while (true)
-      ImgurApi::refreshTokens
-      sleep 45 * 60
-    end
+    p "running"
+    refreshTokens
   end
 end
