891c077ec4f72badc5cfffc24810a33b4221d559
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2015-08-13 10:04:54 +0700

    remove after init rails app

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Fotobook
   class Application < Rails::Application
-    config.eager_load_paths += ["#{config.root}/app/workers"]
-    config.after_initialize do
-      p "--- After Initialize --- "
-      ImgurWorker.perform_async
-      p "--- ImgurWorker is in position."
-    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
