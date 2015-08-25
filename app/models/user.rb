5f03ec94038cbdd646de7c34c4af0add2def0809
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2015-08-25 12:13:24 +0700

    add role to user admin, user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :pictures, class_name: "Picture"
-  ROLES = %i[admin user]
+  ROLES = %i[admin user]  
 end
