af358f3be273512fca7d0eec3373698c452557da
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2015-08-26 15:30:36 +0700

    add searchPictureByTags

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,5 +2,13 @@ class Picture < ActiveRecord::Base
   has_and_belongs_to_many :tags
   belongs_to :user, class_name: "User", foreign_key: "user_id"
   validates :url, presence: true
+
   
+  def self.searchPictureByTags(tag_array)
+    pictures = Picture.joins(:tags).where("tags.tag = #{tag_array.join(',')}")
+    pictures
+  end
+  def self.searchPictureByTagsWildCard(tag_array)
+    
+  end
 end
