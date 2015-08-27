1e10983d63958f18f2613485f3035ade965e1a92
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2015-08-27 17:56:22 +0700

    declare explicitly the join table, f.key, ass.f.key

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,7 @@
 class Picture < ActiveRecord::Base
-  has_and_belongs_to_many :tags
-  belongs_to :user, class_name: "User", foreign_key: "user_id"
+  has_and_belongs_to_many :tags, class_name: "Tag", join_table: "pictures_tags",
+                          foreign_key: "picture_id",
+                          association_foreign_key: "tag_id"
   validates :url, presence: true
 
 
