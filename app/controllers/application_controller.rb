f16c857abcb02be28b83472c32243689509f60d2
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2015-08-30 23:10:52 +0700

    include cancan::additioncontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  include CanCan::ControllerAdditions
   def self.allow_cors(*methods)
     before_filter :cors_before_filter, :only => methods
     protect_from_forgery with: :null_session, :only => methods
   end
 
-   def cors_before_filter
+  def cors_before_filter
     # Check that the `Origin` field matches our front-end client host
-     if /\Ahttps?:\/\/localhost:8000\z/ =~ request.headers['Origin']
-       headers['Access-Control-Allow-Origin'] = request.headers['Origin']
-     end
-   end
-  
+    if /\Ahttps?:\/\/localhost:8000\z/ =~ request.headers['Origin']
+      headers['Access-Control-Allow-Origin'] = request.headers['Origin']
+    end
+  end
 end
