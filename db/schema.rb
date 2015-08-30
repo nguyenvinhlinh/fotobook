1e4a4175f377aa78eba102e56c954ebaea825ff4
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2015-08-30 22:57:36 +0700

    rebase to master, solved confict

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150826182046) do
 
+ActiveRecord::Schema.define(version: 20150825033313) do
   create_table "pictures", force: :cascade do |t|
     t.string   "url"
     t.datetime "created_at", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20150826182046) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
