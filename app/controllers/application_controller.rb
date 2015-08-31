66e700eaa57966861f0af3dc2ee1f37935fba738
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2015-08-31 18:22:32 +0700

    rescue of AccessDenied exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include CanCan::ControllerAdditions
+  
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to new_user_session_path, :alert => exception.message
+  end
+  
   def self.allow_cors(*methods)
     before_filter :cors_before_filter, :only => methods
     protect_from_forgery with: :null_session, :only => methods
