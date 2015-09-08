c17e80a1a6b0f9e19af4cf2030a7df429b36b7f8
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2015-09-08 14:50:42 +0700

    add test admin?

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is_admin" do
+    is_admin_admin = users(:admin).admin?
+    is_user_admin = users(:user).admin?
+    assert(is_admin_admin, "Admin is admin")
+    assert_not(is_user_admin, "User is not an admin")
+  end
 end
