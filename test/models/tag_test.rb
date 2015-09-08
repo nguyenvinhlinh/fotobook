cfe301a75e3a31f558ba00a0c820b5465170c6b4
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2015-09-08 15:14:00 +0700

    add skeleton for tag test

---
+++ test/models/tag_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+class TagTest < ActiveSupport::TestCase
+  test "is_tag_presence" do
+    
+  end
+
+  test "is_tag_unique" do
+  end
+
+  test "is_tag_length_2_5" do
+  end
+end
