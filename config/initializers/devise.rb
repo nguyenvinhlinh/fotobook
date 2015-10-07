5c39b0b956e2f29c1d30fe17f9fff42411d16e77
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2015-10-07 15:34:23 +0700

    Facebook auth works well
    + Add facebook() in omniauth_callback_controller
    + Add configuration in User model
    + Add from_omniauth(), new_with_session()
    + add omniauth_callbacks

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -237,7 +237,8 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, "1704587769771562",
-                  "b38ca1660f683c4273395ff3a2345b79", scope: 'email'
+                  "b38ca1660f683c4273395ff3a2345b79",
+                  scope: 'email, public_profile'
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-               sessions: 'users/sessions'
+  devise_for :users,
+             controllers: {
+               sessions: 'users/sessions',
+               omniauth_callbacks: "users/omniauth_callbacks"
              }
   root "pictures#index"
   resources :pictures,  only: [:show, :index, :new] do
